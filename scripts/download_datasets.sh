e5c890cd8bf9080915ec773ae4befb55fb454e6a
msrepo <mahesh.shakya@naamii.org.np>
2024-01-06 16:22:11 +0545

    download datasets

---
+++ scripts/download_datasets.sh
@@ -0,0 +1,8 @@
+[ -d odir2019 ] || mkdir odir2019
+cd odir2019
+gdown 1UGrMGfb9zvbBqOvbV62G-XdUlBIAvOad&confirm=t 
+gdown 19OD9a29nrSbLC2Pch4UZtpp8qtFZLd-y&confirm=t
+gdown 1rQCwZYq3Iu02uU9UzGQ1HdWqU_Xuwfx2&confirm=t
+gdown 1KteV8Z5fJ0kD9i64bQ4OAgW8bH-889gT&confirm=t
+gdown 1OkysN9vD7ZRQdIKv-TgqH3_Wh0Zp9YTZ&confirm=t
+gdown 1jc7Dmp26km0PKRwf9u3Xcyui4SRiojcT&confirm=t
